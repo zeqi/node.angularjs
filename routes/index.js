786d71b7c9299323455d0b61321d3524f921fa97
zhuxijun <zhuxijun@b2cf.cn>
2017-04-21 19:35:44 +0800

    add socket.io test module

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',socketServer:"127.0.0.1:3000" });
+  res.render('index', { title: 'Express',socketServer:"127.0.0.1:3001" });
 });
 
 module.exports = router;
